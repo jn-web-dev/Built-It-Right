4f27870803c42c4510ebeb62120f485f703d981b
John <john.ngethe19@students.dkut.ac.ke>
2024-10-07 17:35:00 +0300

    add hero section and navigation menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max max-w-7xl w-full">
+          <Nav />
           <Hero />
+          
         </div>
       </main>
     
